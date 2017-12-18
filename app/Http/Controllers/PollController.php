dc2d6f8d06e3a193b6fbc7666e95c2dc467db4ab
marmargal <inma.martin.galvan@hotmail.com>
2017-12-18 11:12:13 +0100

    feat: implementación del método getOptionsByPoll
    Se ha añadido a PollController el método getOptionsByPoll y algunas correcciones de los métodos anteriores.

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -3,8 +3,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\UserAuthPoll;
 use App\Poll;
-use App\Vote;
 
 
 class PollController extends Controller
@@ -15,28 +15,46 @@ class PollController extends Controller
         return response()->json($polls, 200);
     }
 
-    public function getPollByUserAuthoritation($id,$auth_token)
+
+    public function getPollByUserAuthoritation($id, $auth)
     {
-
-        if($id == null || $id= "" || $auth_token == null || $auth_token = ""){
+        if ($id == null || $id == "" || $auth == null || $auth == "") {
             abort(400, 'Bad request');
-
-        }else{
-
-            $res = app('db')->select("SELECT * FROM user_auth_poll where poll_id=".$id . " and auth_token= ".$auth_token);
-
-            if($res != null){
-                return response()->json($res, 200);
-
-            }else{
+        } else {
+            $pollauthuser = UserAuthPoll::where('poll_id', '=', $id)->Where('auth_token', '=', $auth)->get();
+            if ($pollauthuser == null || count($pollauthuser) == 0) {
                 abort(403, 'Unauthorized action.');
-
+            } else {
+                $poll_id = 0;
+                foreach ($pollauthuser as $key) {
+                    $poll_id = $key->poll_id;
+                }
+                $res = Poll::where('poll_id', '=', $poll_id)->get();
+                return response()->json($res, 200);
             }
         }
-
-
     }
 
+
+    public function getAllPollByUserAuthoritation($auth)
+    {
+        if ($auth == null || $auth == "") {
+            abort(400, 'Bad request');
+        } else {
+            $pollsAuthuser = UserAuthPoll::where('auth_token', '=', $auth)->get();
+            if ($pollsAuthuser == null || count($pollsAuthuser) == 0) {
+                abort(403, 'Unauthorized action.');
+            } else {
+                $poll_id = 0;
+                $res = array();
+                foreach ($pollsAuthuser as $key) {
+                    $poll_id = $key->poll_id;
+                    array_push($res, Poll::where('poll_id', '=', $poll_id)->get());
+                }
+                return response()->json($res, 200);
+            }
+        }
+    }
 
     /*public function createCategories(Request $request)
     {
@@ -44,6 +62,7 @@ class PollController extends Controller
         return response()->json($categoria,201);
     }*/
 
+    
     public function getVotesByPoll($id,$auth)
     {
         if ($id==null || $id=="" || $auth==null || $auth == ""){
@@ -97,5 +116,33 @@ class PollController extends Controller
         }
     }
 
+
+    public function getOptionsByPoll($id,$auth)
+    {
+        if($id == null || $id== "" || $auth == null || $auth == ""){
+            abort(400, 'Bad request');
+        }else{
+            $pollauthuser = UserAuthPoll::where('poll_id','=',$id)->Where('auth_token','=',$auth)->get();
+            if($pollauthuser == null || count($pollauthuser) == 0){
+                abort(403, 'Unauthorized action.');
+            }else{
+                $poll_id=0;
+                foreach($pollauthuser as $key){
+                    $poll_id = $key->poll_id;
+                }
+                $res = DB::table('Poll_Option_Poll')->select(DB::raw('poll_option_id'))->where('poll_id','=',$poll_id)->get();
+                $resFinal = array();
+                foreach($res as $value){
+                    //var_dump($value->poll_option_id);
+                    $res1 = DB::table('Poll_Option')->select(DB::raw('option'))->where('poll_option_id','=',$value->poll_option_id)->get();
+                    foreach($res1 as $value1){
+                        //var_dump($value1);
+                        array_push($resFinal,$value1);
+                    }
+                }
+                return response()->json($resFinal, 200);
+            }
+        }
+    }
 
 }
