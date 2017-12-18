ba7fee18e6ca43cb0fb7b9464522bfa0a67a0810
marmargal <inma.martin.galvan@hotmail.com>
2017-12-18 09:58:23 +0100

    feat: implementación del método getVotesByPoll
    Se ha añadido el método getVotesByPoll

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Poll;
+use App\Vote;
 
 
 class PollController extends Controller
@@ -42,5 +43,35 @@ class PollController extends Controller
         $categoria = Categories::create($request->all());
         return response()->json($categoria,201);
     }*/
+
+    public function getVotesByPoll($id,$auth)
+    {
+        if ($id==null || $id=="" || $auth==null || $auth == ""){
+            abort(400,'Bad request');
+        }else{
+            $pollauthuser = UserAuthPoll::wher('poll_id','=',$id)->Where('auth_token','=',$auth)->get();
+
+            if($pollauthuser==null || count($pollauthuser)==0){
+                abort(403,'Unauthorized action.');
+            }else{
+                $poll_id=0;
+                foreach ($pollauthuser as $key){
+                    $poll_id = $key->poll_id;
+                }
+                $res = DB::table('Poll_Option_Poll')->select(DB::raw('poll_option_id'))->where('poll_id','=',$poll_id)->get();
+                $resFinal = array();
+
+                foreach ($res as $value){
+                    $res1 = DB::table('Poll_Option')->select(DB::raw('option'))->where('poll_option_id','=',$value->poll_option_id)->get();
+                     foreach ($res1 as $value1){
+                         array_push($resFinal,$value1);
+                     }
+                }
+                return response()->json($resFinal, 200);
+            }
+
+        }
+
+    }
 
 }
