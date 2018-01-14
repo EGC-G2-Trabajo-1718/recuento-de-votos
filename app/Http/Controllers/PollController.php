0e16516ab343f6fa4a3d3503e629cd20aec1ee9c
Jesús Barrera <abarreraroldan@gmail.com>
2018-01-14 17:12:18 +0100

    Se añade método para realizar el recuento de voto y método para saber las opciones votadas en función de los parametros de entrada recibidos por la api de almacenamiento

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -2,45 +2,115 @@
 
 
 namespace App\Http\Controllers;
+use App\EncryptUtil;
+use App\PollOptionPoll;
 use Illuminate\Http\Request;
 use App\Poll;
+use App\UserAuthPoll;
+use App\Vote;
+use App\DescryptUtil;
+
+
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Collection;
+
+
 
 
 class PollController extends Controller
 {
-    public function index()
-    {
-        $polls = Poll::all();
-        return response()->json($polls, 200);
-    }
 
-    public function getPollByUserAuthoritation($id,$auth_token)
+
+    public function getVotesByPoll($token_bd,$token_votacion,$token_pregunta)
     {
 
-        if($id == null || $id= "" || $auth_token == null || $auth_token = ""){
+        if($token_votacion == null || $token_votacion== "" || $token_pregunta == null || $token_pregunta == ""){
             abort(400, 'Bad request');
+            error_log("Error 400, alguna variable vacia o nula");
 
         }else{
 
-            $res = app('db')->select("SELECT * FROM user_auth_poll where poll_id=".$id . " and auth_token= ".$auth_token);
+            try{
 
-            if($res != null){
-                return response()->json($res, 200);
+                $url_api = env('URL_ALMACENAMIENTO');
+                $url_api = $url_api ."/" . $token_bd . "/" . $token_votacion . "/" . $token_pregunta;
 
-            }else{
-                abort(403, 'Unauthorized action.');
+                $json = file_get_contents($url_api);
+                $obj = json_decode($json);
+                $descryptUtil = new DescryptUtil();
+                $res = array();
+
+
+                foreach($obj as $value){
+                    array_push($res,$descryptUtil->descrypt($value->token_respuesta));
+
+                }
+                $resFinal = array_count_values($res);
+                $result = array("total_votes" => sizeof($res));
+
+                $encryptUtil = new EncryptUtil();
+                foreach($resFinal as $key=>$value){
+                    $result[$encryptUtil->encrypt($key)] = $value ;
+                }
+
+
+                return response()->json($result, 200);
+            }catch(Exception $e){
+                    error_log("Se ha producido un error no controlado en PollController",$e);
+                }
 
             }
+
         }
 
+
+
+    public function getOptionsByPoll($token_bd,$token_votacion,$token_pregunta)
+    {
+
+        if($token_votacion == null || $token_votacion== "" || $token_pregunta == null || $token_pregunta == ""){
+            abort(400, 'Bad request');
+            error_log("Error 400, alguna variable vacia o nula");
+
+        }else{
+
+            try{
+                $url_api = env('URL_ALMACENAMIENTO');
+                $url_api = $url_api ."/" . $token_bd . "/" . $token_votacion . "/" . $token_pregunta;
+
+                $json = file_get_contents($url_api);
+
+
+                $obj = json_decode($json);
+                $descryptUtil = new DescryptUtil();
+                $res = array();
+
+
+                foreach($obj as $value){
+                    array_push($res,$descryptUtil->descrypt($value->token_respuesta));
+
+                }
+                $resFinal = array_count_values($res);
+
+                $encryptUtil = new EncryptUtil();
+                $result = array('options');
+                foreach($resFinal as $key=>$value){
+                    array_push($result,$encryptUtil->encrypt($key));
+
+                }
+
+
+                return response()->json($result, 200);
+            }catch(Exception $e){
+                error_log("Se ha producido un error no controlado en PollController",$e);
+            }
+
+        }
 
     }
 
 
-    /*public function createCategories(Request $request)
-    {
-        $categoria = Categories::create($request->all());
-        return response()->json($categoria,201);
-    }*/
+
 
 }
