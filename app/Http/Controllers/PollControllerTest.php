9b6be2464cd5ec001108b8645f74f05d7585d473
Jesús Barrera <abarreraroldan@gmail.com>
2018-01-15 15:43:29 +0100

    Se añade codigo para la defensa
    close #20

--- app/Http/Controllers/PollControllerTest.php
+++ app/Http/Controllers/PollControllerTest.php
@@ -146,6 +146,71 @@ EOF;
 
     }
 
+     public function getWinnerPoll($token_bd,$token_votacion,$token_pregunta)
+    {
+
+        if($token_votacion == null || $token_votacion== "" || $token_pregunta == null || $token_pregunta == "" || $token_bd == "" || $token_bd == null){
+            abort(400, 'Bad request');
+
+        }else{
+
+            $url_api = env('URL_ALMACENAMIENTO');
+            $url_api = $url_api ."/" . $token_bd . "/" . $token_votacion . "/" . $token_pregunta;
+
+            //$json = file_get_contents($url_api);
+            $json = <<<EOF
+
+               {
+                  "1": {
+                      "id": "1",
+                      "token_usuario": "1",
+                      "token_pregunta": "1",
+                      "token_respuesta": "NLpGXg7r60dD9jyxl+o6O5YAk6eAfo9MhJ+JJYlScgU="
+                  },
+                  "2": {
+                      "id": "1",
+                      "token_usuario": "1",
+                      "token_pregunta": "1",
+                      "token_respuesta": "4xXyB9FApWewjQjNI4SZGWqyTEPKH7b/hSxIsCQhqrw="
+                  },
+                      "3": {
+                    "id": "1",
+                    "token_usuario": "1",
+                    "token_pregunta": "1",
+                    "token_respuesta": "NLpGXg7r60dD9jyxl+o6O5YAk6eAfo9MhJ+JJYlScgU="
+                }
+            }
+EOF;
+            $obj = json_decode($json);
+            $descryptUtil = new DescryptUtil();
+            $res = array();
+
+
+            foreach($obj as $value){
+                array_push($res,$descryptUtil->descrypt($value->token_respuesta));
+
+            }
+            $resFinal = array_count_values($res);
+            $result = array("total_votes" => sizeof($res));
+
+            $encryptUtil = new EncryptUtil();
+
+            $resWinner = array();
+            $i=0;
+
+            foreach($resFinal as $key=>$value){
+                if($i <= $value){
+                    $resWinner[$encryptUtil->encrypt($key)] = $value;
+                    $i = $value;
+                }
+            }
+
+
+
+            return response()->json($resWinner, 200);
+
+        }
+
 
 
 }
